93bc88e2c779119f95a5e006ffd53baf18e6a338
ruslan <zrm1306@gmail.com>
2023-11-14 19:07:50 +0500

    change app rotes

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {ROUTES} from '../../utils/routes'
 import Home from "../Home/Home";
 import SingleBook from "../BookList/SingleBook";
@@ -7,6 +7,7 @@ const AppRoutes = () => (
     <Routes>
         <Route path={ROUTES.HOME} element={<Home/>}/>
         <Route path={ROUTES.SINGLE_BOOK} element={<SingleBook />}/>
+        <Route path={'*'} element={<Navigate to={ROUTES.HOME} />}/>
     </Routes>
 )
 export default AppRoutes
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,4 +1,4 @@
 export const ROUTES = {
-    HOME: '/',
+    HOME: '/book',
     SINGLE_BOOK: '/book/:id'
 }
